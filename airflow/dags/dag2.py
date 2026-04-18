a623783c34832c80051d22615706d7da759634a7
arnavgupta <arnavgupta295@gmail.com>
2026-04-18 17:21:43 +0530

    Adding sowflake executor taks in dag2 folder

--- airflow/dags/dag2.py
+++ airflow/dags/dag2.py
@@ -27,49 +27,6 @@ default_args = {
 }
 
 
-# with DAG(
-#     dag_id="safecity360_abhinav_pipeline",
-#     schedule=None,
-#     start_date=datetime(2025, 1, 1),
-#     catchup=False,
-# ) as dag:
-
-
-
-
-
-
-# ============================================================
-# FUNCTION: Upload batch CSV from local folder to S3
-# ============================================================
-# def upload_batch_csv_to_s3(**kwargs):
-#     """
-#     Reads the batch CSV file from the local folder (mounted from Mac)
-#     and uploads it to the correct S3 path using boto3.
-#     """
-#     batch_id = kwargs["batch_id"]
-#     bucket = kwargs["bucket"]
-#     project = kwargs["project"]
-
-
-#     local_file = f"{LOCAL_BATCH_DIR}/crimes_2020_01.csv"
-#     # local_file = f"{LOCAL_BATCH_DIR}/crimes_{batch_id}.csv"
-#     s3_key = f"{project}/batch_data/{batch_id}/crimes_{batch_id}.csv"
-
-#     if not os.path.exists(local_file):
-#         raise FileNotFoundError(
-#             f"Batch file not found: {local_file}\n"
-#             f"Make sure you copied the CSV to your batch_data/ folder:\n"
-#             f"  cp /path/to/crimes_{batch_id}.csv batch_data/"
-#         )
-
-#     s3 = boto3.client("s3", aws_access_key_id=AWS_KEY,
-#         aws_secret_access_key=AWS_SECRET, region_name=REGION)
-
-#     file_size = os.path.getsize(local_file)
-#     print(f"Uploading {local_file} ({file_size} bytes) to s3://{bucket}/{s3_key}")
-#     s3.upload_file(local_file, bucket, s3_key)
-#     print(f"Upload complete: s3://{bucket}/{s3_key}")
 
 # ============================================================
 # DAG DEFINITION
@@ -147,79 +104,12 @@ with DAG(
 )
 
 
-#     # run_silver = DatabricksSubmitRunOperator(
-#     #     task_id="run_silver", databricks_conn_id="databricks_default",
-#     #     notebook_task={"notebook_path": f"{NOTEBOOK_ROOT}/02_silver"})
 
-#     # TASK 4: Gold
-
-#     run_gold = DatabricksSubmitRunOperator(
-#     task_id="run_gold",
-#     databricks_conn_id="databricks_default",
-#     json={
-#         "tasks": [
-#             {
-#                 "task_key": "gold_task",
-#                 "notebook_task": {
-#                     "notebook_path": "/Workspace/Users/abhinav66623@gmail.com/SafeCity_1/03_gold"
-#                 }
-#             }
-#         ]
-#     }
-# )
-    
-#     # run_gold = DatabricksSubmitRunOperator(
-#     #     task_id="run_gold", databricks_conn_id="databricks_default",
-#     #     notebook_task={"notebook_path": f"{NOTEBOOK_ROOT}/03_gold"})
-
-#     # TASK 5: DQ Checks
-
-#     run_dq = DatabricksSubmitRunOperator(
-#     task_id="run_dq",
-#     databricks_conn_id="databricks_default",
-#     json={
-#         "tasks": [
-#             {
-#                 "task_key": "dq",
-#                 "notebook_task": {
-#                     "notebook_path": "/Workspace/Users/abhinav66623@gmail.com/SafeCity_1/04_dq"
-#                 }
-#             }
-#         ]
-#     }
-# )
-    
-#     # TASK 6: Export Gold to S3
-#     export_gold_to_s3 = DatabricksSubmitRunOperator(
-#     task_id="export_gold_to_s3",
-#     databricks_conn_id="databricks_default",
-#     json={
-#         "tasks": [
-#             {
-#                 "task_key": "export_gold_to_s3",
-#                 "notebook_task": {
-#                     "notebook_path": f"{NOTEBOOK_ROOT}/05_export_gold_to_s3"
-#                 }
-#             }
-#         ]
-#     }
-# )
-
-#     # TASK 7: Refresh Snowflake
-#     refresh_snowflake = SQLExecuteQueryOperator(
-#         task_id="refresh_snowflake", conn_id="snowflake_default",
-#         sql="""USE DATABASE SAFECITY360_DB; USE SCHEMA STAGING; USE WAREHOUSE SAFECITY360_WH;
-#             TRUNCATE TABLE IF EXISTS STAGING.DIM_DATE; COPY INTO STAGING.DIM_DATE FROM @STAGING.SAFECITY360_STAGE/dim_date.parquet FILE_FORMAT=(TYPE='PARQUET') MATCH_BY_COLUMN_NAME=CASE_INSENSITIVE;
-#             TRUNCATE TABLE IF EXISTS STAGING.DIM_DISTRICT; COPY INTO STAGING.DIM_DISTRICT FROM @STAGING.SAFECITY360_STAGE/dim_district.parquet FILE_FORMAT=(TYPE='PARQUET') MATCH_BY_COLUMN_NAME=CASE_INSENSITIVE;
-#             TRUNCATE TABLE IF EXISTS STAGING.DIM_IUCR_CODE; COPY INTO STAGING.DIM_IUCR_CODE FROM @STAGING.SAFECITY360_STAGE/dim_iucr_code.parquet FILE_FORMAT=(TYPE='PARQUET') MATCH_BY_COLUMN_NAME=CASE_INSENSITIVE;
-#             TRUNCATE TABLE IF EXISTS STAGING.DIM_LOCATION_TYPE; COPY INTO STAGING.DIM_LOCATION_TYPE FROM @STAGING.SAFECITY360_STAGE/dim_location_type.parquet FILE_FORMAT=(TYPE='PARQUET') MATCH_BY_COLUMN_NAME=CASE_INSENSITIVE;
-#             TRUNCATE TABLE IF EXISTS STAGING.DIM_TIME_BLOCK; COPY INTO STAGING.DIM_TIME_BLOCK FROM @STAGING.SAFECITY360_STAGE/dim_time_block.parquet FILE_FORMAT=(TYPE='PARQUET') MATCH_BY_COLUMN_NAME=CASE_INSENSITIVE;
-#             TRUNCATE TABLE IF EXISTS STAGING.FACT_CRIME_INCIDENTS; COPY INTO STAGING.FACT_CRIME_INCIDENTS FROM @STAGING.SAFECITY360_STAGE/fact_crime_incidents.parquet FILE_FORMAT=(TYPE='PARQUET') MATCH_BY_COLUMN_NAME=CASE_INSENSITIVE;
-#             TRUNCATE TABLE IF EXISTS STAGING.FACT_MONTHLY_DISTRICT_SUMMARY; COPY INTO STAGING.FACT_MONTHLY_DISTRICT_SUMMARY FROM @STAGING.SAFECITY360_STAGE/fact_monthly_district_summary.parquet FILE_FORMAT=(TYPE='PARQUET') MATCH_BY_COLUMN_NAME=CASE_INSENSITIVE;
-#         """)
-
-#     # PIPELINE ORDER (7 tasks)
-#     upload_csv_to_s3 >> run_bronze >> run_silver >> run_gold >> run_dq >> export_gold_to_s3 >> refresh_snowflake
+    snowflake_step = SQLExecuteQueryOperator(
+        task_id="snowflake_transformation",
+        conn_id="snowflake_default",
+        sql="sql/snowflake_transformations.sql"
+    )
 
 
-run_bronze >> run_silver >> run_silver_dq
+run_bronze >> run_silver >> run_silver_dq >> snowflake_step
